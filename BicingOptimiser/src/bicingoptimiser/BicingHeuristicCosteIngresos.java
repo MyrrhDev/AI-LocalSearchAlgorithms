ac3b9e002592faba2b1577d88aceef16f05c4a50
Mayra Pastor Valdivia <mayra.pastor@est.fib.upc.edu>
2019-10-24 18:40:15 +0200

    Create BicingHeuristicCosteIngresos.java
    Heuristico Coste ingresos

---
+++ BicingOptimiser/src/bicingoptimiser/BicingHeuristicCosteIngresos.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bicingoptimiser;
+
+import aima.search.framework.HeuristicFunction;
+/**
+ *
+ * @author Josep Clotet Ginovart, Fernando Marimon Llopis, Mayra Pastor Valdivia
+ */
+public class BicingHeuristicCosteIngresos implements HeuristicFunction {
+// Aquest valora els costos. verificar que se puede con int la sig. funcion:
+		public double getHeuristicValue(Object state) {
+			BicingOptimiserState estado = (BicingOptimiserState ) state;
+                        return estado.getCosteDistancia() - estado.getIngresos();
+		}
+}
