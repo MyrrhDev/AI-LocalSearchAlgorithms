c838398d6781ca980f8d7771088e05c78adb7cd5
Josep <josep.clotet.ginovart@est.fib.upc.edu>
2019-10-27 13:42:40 +0100

    bucles, interacción usuario, tiempos ejecución

--- BicingOptimiser/src/bicingoptimiser/Main.java
+++ BicingOptimiser/src/bicingoptimiser/Main.java
@@ -11,6 +11,7 @@ import aima.search.framework.Search;
 import aima.search.framework.SearchAgent;
 import aima.search.informed.HillClimbingSearch;
 import aima.search.informed.SimulatedAnnealingSearch;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
@@ -36,96 +37,165 @@ public class Main {
         int boolIniSimple;
         int boolProporcionalidad;
         
+        Long startTime, stopTime;
+        List<Long> elapsedTime = new ArrayList<Long>();
+        
+        int numIter; int bucle = 0;
         Scanner in = new Scanner(System.in);
-        System.out.println("Rush hour? (Sí: 1, No: 0): ");
-        demanda = in.nextInt();
-        System.out.println("Proporcionalidad entre bicis-estacions-furgonetas? (Sí: 1, No: 0): ");
-        boolProporcionalidad = in.nextInt();
-        System.out.println("Num Estaciones: ");    
-        numEstaciones = in.nextInt();
-        if(boolProporcionalidad==1){
-            numBicis = 50*numEstaciones;
-            maxFurgos = numEstaciones/5;
-        }
-        else{
-            System.out.println("Num Bicis: ");    
-            numBicis = in.nextInt();
-            System.out.println("Num Furgos: "); 
-            maxFurgos = in.nextInt();
-        }
- 
-        System.out.println("Seed: "); 
-        seed = in.nextInt();
+        System.out.println("Número de ejecuciones: ");
+        numIter = in.nextInt();
+        System.out.println("Introducir parámetros de uno en uno (1) o separados por comas (2): ");
+        int method;
+        method = in.nextInt();
         
-        System.out.println("Hill Climbing: 1, Simulated Annealing: 2: ");
-        boolHill = in.nextInt();
-        
-        System.out.println("Heurístico Simple: 1, Heurístico Complejo: 2: ");
-        boolHeurSimple = in.nextInt();
-        
-        System.out.println("Estado inicial vacío: 1, Estado inicial lleno: 2: ");
-        boolIniSimple = in.nextInt();
-        
-        Estaciones estaciones = new Estaciones(numEstaciones, numBicis, demanda, seed);
-        BicingOptimiserState estado = new BicingOptimiserState(estaciones, maxFurgos);
-        
-        if(boolIniSimple==1)
-            estado.solucionInicialSimple();
-        else
-            estado.solucionInicialCompleja();
-        
-        BicingOptimiserState q;
-        Problem prob;
-        
-        try{
-            if(boolHeurSimple==1){
-                if(boolHill==1)
-                    prob = new Problem(estado, new BicingSuccessorsHillClimbing(), new BicingOptimiserGoalTest(), new BicingHeuristicIngresos());
-                else
-                    prob = new Problem(estado, new BicingSuccessorsSimAnnealing(), new BicingOptimiserGoalTest(), new BicingHeuristicIngresos());
+            if(method == 1){
+                System.out.println("Rush hour? (Sí: 1, No: 0): ");
+                demanda = in.nextInt();
+                System.out.println("Proporcionalidad entre bicis-estacions-furgonetas? (Sí: 1, No: 0): ");
+                boolProporcionalidad = in.nextInt();
+                System.out.println("Num Estaciones: ");    
+                numEstaciones = in.nextInt();
+                if(boolProporcionalidad==1){
+                    numBicis = 50*numEstaciones;
+                    maxFurgos = numEstaciones/5;
+                }
+                else{
+                    System.out.println("Num Bicis: ");    
+                    numBicis = in.nextInt();
+                    System.out.println("Num Furgos: "); 
+                    maxFurgos = in.nextInt();
+                }
+
+                System.out.println("Seed: "); 
+                seed = in.nextInt();
+
+                System.out.println("Hill Climbing: 1, Simulated Annealing: 2: ");
+                boolHill = in.nextInt();
+
+                System.out.println("Heurístico Simple: 1, Heurístico Complejo: 2: ");
+                boolHeurSimple = in.nextInt();
+
+                System.out.println("Estado inicial vacío: 1, Estado inicial lleno: 2: ");
+                boolIniSimple = in.nextInt();
             }
             else{
-                if(boolHill==1)
-                    prob = new Problem(estado, new BicingSuccessorsHillClimbing(), new BicingOptimiserGoalTest(), new BicingHeuristicCosteIngresos());
-                else
-                    prob = new Problem(estado, new BicingSuccessorsSimAnnealing(), new BicingOptimiserGoalTest(), new BicingHeuristicCosteIngresos());
-            }
-            
-            Search search;
-            if(boolHill==1)
-                search= new HillClimbingSearch();
-            else
-                search = new SimulatedAnnealingSearch(2000,100,2,0.0001);
-            
-            SearchAgent agent = new SearchAgent(prob,search);
-            
-            if(boolHill==1){
-                printActions(agent.getActions());
-                System.out.println("");
-                printInstrumentation(agent.getInstrumentation());
-                System.out.println("");
-            }
-            
-            q = (BicingOptimiserState) search.getGoalState();
-            System.out.println("==========================================================");
-            System.out.println("");
-            System.out.println("SOLUCIÓN:");
-            System.out.println("");
-            System.out.println("Ingresos: "+q.getIngresos());
-            System.out.println("Costes: "+q.calcularCosteDistancia());
-            System.out.println("Ingresos - Costes: "+(q.getIngresos()-q.calcularCosteDistancia()) );
-            System.out.println("");
+                System.out.println("INTRODUCIR VALORES SEPARADOS POR COMAS EN ESTE ORDEN: ");
+                System.out.println("Rush hour? (Sí: 1, No: 0): ");
+                System.out.println("Proporcionalidad entre bicis-estacions-furgonetas? (Sí: 1, No: 0): ");
+                System.out.println("Num Estaciones: ");
+                System.out.println("    Si no se requiere proporcionalidad -> Num Bicis: ");    
+                System.out.println("    Si no se requiere proporcionalidad -> Num Furgos: "); 
+                System.out.println("Seed: ");
+                System.out.println("Hill Climbing: 1, Simulated Annealing: 2: ");
+                System.out.println("Heurístico Simple: 1, Heurístico Complejo: 2: ");
+                System.out.println("Estado inicial vacío: 1, Estado inicial lleno: 2: ");
+                
+                in.nextLine(); //nextInt() does not consume the '\n', so the next call to nextLine() consumes it and then it's waiting to read the input 
+                String input = in.nextLine();
+                String[] inputArray = input.split(",");
 
-            //System.out.print(((BicingOptimiserState) search.getGoalState()).toString());
-            System.out.println("=========================================================="); 
-            System.out.println(q.toString());
+                int orden = 0;
+                demanda = Integer.parseInt(inputArray[orden]); orden++;
+                boolProporcionalidad = Integer.parseInt(inputArray[orden]); orden++;
+                numEstaciones = Integer.parseInt(inputArray[orden]); orden++;
+                if(boolProporcionalidad == 1){
+                    numBicis = 50*numEstaciones;
+                    maxFurgos = numEstaciones/5;
+                    seed = Integer.parseInt(inputArray[orden]); orden++;
+                    boolHill = Integer.parseInt(inputArray[orden]); orden++;
+                    boolHeurSimple = Integer.parseInt(inputArray[orden]); orden++;
+                    boolIniSimple = Integer.parseInt(inputArray[orden]); orden++;
+                }
+                else{
+                    numBicis = Integer.parseInt(inputArray[orden]); orden++;
+                    maxFurgos = Integer.parseInt(inputArray[orden]);orden++;
+                    seed = Integer.parseInt(inputArray[orden]); orden++;
+                    boolHill = Integer.parseInt(inputArray[orden]); orden++;
+                    boolHeurSimple = Integer.parseInt(inputArray[orden]); orden++;
+                    boolIniSimple = Integer.parseInt(inputArray[orden]); orden++;
+                }
+            }
         
+        System.out.println("=============================START=============================");
+        do {
+            System.out.println("============================ejecución "+(bucle+1)+"============================");
+            startTime = System.currentTimeMillis();
+            Estaciones estaciones = new Estaciones(numEstaciones, numBicis, demanda, seed);
+            BicingOptimiserState estado = new BicingOptimiserState(estaciones, maxFurgos);
+
+            if(boolIniSimple==1)
+                estado.solucionInicialSimple();
+            else
+                estado.solucionInicialCompleja();
+
+            BicingOptimiserState q;
+            Problem prob;
+
+            try{
+                if(boolHeurSimple==1){
+                    if(boolHill==1)
+                        prob = new Problem(estado, new BicingSuccessorsHillClimbing(), new BicingOptimiserGoalTest(), new BicingHeuristicIngresos());
+                    else
+                        prob = new Problem(estado, new BicingSuccessorsSimAnnealing(), new BicingOptimiserGoalTest(), new BicingHeuristicIngresos());
+                }
+                else{
+                    if(boolHill==1)
+                        prob = new Problem(estado, new BicingSuccessorsHillClimbing(), new BicingOptimiserGoalTest(), new BicingHeuristicCosteIngresos());
+                    else
+                        prob = new Problem(estado, new BicingSuccessorsSimAnnealing(), new BicingOptimiserGoalTest(), new BicingHeuristicCosteIngresos());
+                }
+
+                Search search;
+                if(boolHill==1)
+                    search= new HillClimbingSearch();
+                else
+                    search = new SimulatedAnnealingSearch(2000,100,2,0.0001);
+
+                SearchAgent agent = new SearchAgent(prob,search);
+
+                if(boolHill==1){
+                    printActions(agent.getActions());
+                    System.out.println("");
+                    printInstrumentation(agent.getInstrumentation());
+                    System.out.println("");
+                }
+
+                q = (BicingOptimiserState) search.getGoalState();
+                System.out.println("==========================================================");
+                System.out.println("");
+                System.out.println("SOLUCIÓN:");
+                System.out.println("");
+                System.out.println("Ingresos: "+q.getIngresos());
+                System.out.println("Costes: "+q.calcularCosteDistancia());
+                System.out.println("Ingresos - Costes: "+(q.getIngresos()-q.calcularCosteDistancia()) );
+                System.out.println("");
+
+                //System.out.print(((BicingOptimiserState) search.getGoalState()).toString());
+                System.out.println("=========================================================="); 
+                System.out.println(q.toString());
+
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            stopTime = System.currentTimeMillis();
+            elapsedTime.add(stopTime - startTime);
+
+            bucle++;   
+        } while (bucle < numIter);  
+        
+        System.out.println("========================tiempos de ejecución========================");
+        Long sum = 0L;
+        for(int i = 0; i < elapsedTime.size(); i++){
+            if(i != 0){ //la primera ejecución se descarta por temas de caché
+                System.out.println("ejecución "+i+": "+elapsedTime.get(i)+" ms");
+                sum += elapsedTime.get(i);
+            }
         }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        System.out.println("TIEMPO MEDIO: "+sum/(elapsedTime.size()-1)+" ms");
         
-        
+        System.out.println("=============================END=============================");
     }
     
      private static void printActions(List actions) {
@@ -144,5 +214,5 @@ public class Main {
         }
 
     }
-            
+    
 }
